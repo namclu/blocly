c2610fb07638229013ab6408a249bfbe159090c7
Nam <namlu1205@gmail.com>
2016-06-16 14:35:54 +1000

    Checkpoint 32 completed

---
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,17 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
+
+/**
+ * Created by namlu on 14-Jun-16.
+ */
+public class BloclyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
+}
