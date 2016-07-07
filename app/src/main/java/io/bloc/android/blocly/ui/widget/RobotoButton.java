c4185589cce1d641a694786221d2ec624526497d
Nam <namlu1205@gmail.com>
2016-07-07 10:49:22 +1000

    Assignment 40 p_view_manipulation: add RobotoButton.java, v21/btn_background_ripple.xml, but error at runtime

---
+++ app/src/main/java/io/bloc/android/blocly/ui/widget/RobotoButton.java
@@ -0,0 +1,26 @@
+package io.bloc.android.blocly.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by namlu on 06-Jul-16.
+ */
+public class RobotoButton extends Button {
+    public RobotoButton(Context context) {
+        super(context);
+    }
+
+    public RobotoButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RobotoButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RobotoButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
