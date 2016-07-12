0dcd7d6aa631138fe49b58cc54e84c26a28f4e12
Nam <namlu1205@gmail.com>
2016-07-12 16:18:59 +1000

    Checkpoint 42 drawer layout: completed

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,10 +1,15 @@
 package io.bloc.android.blocly.ui.activity;
 
-import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.ui.adapter.ItemAdapter;
@@ -12,14 +17,21 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 /**
  * Created by namlu on 14-Jun-16.
  */
-public class BloclyActivity extends Activity {
+public class BloclyActivity extends AppCompatActivity {
 
     private ItemAdapter itemAdapter;
+    // Add to use DrawerLayout
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
+
+        // Assign ToolBar as our ActionBar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_activity_blocly);
+        setSupportActionBar(toolbar);
 
         itemAdapter = new ItemAdapter();
 
@@ -29,5 +41,41 @@ public class BloclyActivity extends Activity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(itemAdapter);
+
+        // Recover the instance of ActionBar associated with ToolBar
+        // and invoke setDisplayHomeAsUpEnabled(boolean) to allow this behavior
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = (DrawerLayout) findViewById(R.id.dl_activity_blocly);
+
+        // ActionBarDrawerToggle(activity, drawerLayout, int openDrawerContentDescRes, int closeDrawerContentDescRes)
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, 0, 0);
+        drawerLayout.addDrawerListener(drawerToggle);
+    }
+
+    @Override
+    // Call syncState() to synchronize the indicator with the state of the linked DrawerLayout
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    // Called when the device configuration changes while your activity is running
+    // This will only be called if you have selected configurations you would like
+    // to handle with the configChanges attribute in your manifest
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    // This hook is called whenever an item in your options menu is selected
+    // Default implementation simply returns false to have the normal processing happen
+    // (calling the item's Runnable or sending a message to its Handler as appropriate)
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
