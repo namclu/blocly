281f2bbf7c86a93b7a9c25f8c802bcf1bfb42ea9
namclu <namlu1205@gmail.com>
2017-01-12 21:33:31 +1100

    43 drawer layout 2: define onBindViewHolder() to add RSS feeds to view, update() to get position and update the nav drawer as user scrolls through feeds

--- app/src/main/java/io/bloc/android/blocly/ui/adapter/NavigationDrawerAdapter.java
+++ app/src/main/java/io/bloc/android/blocly/ui/adapter/NavigationDrawerAdapter.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.api.model.RssFeed;
 
 /**
  * Created by namlu on 12-Jan-17.
@@ -34,6 +35,15 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
 
+        RssFeed rssFeed = null;
+
+        // 43.6: If view position is beyond NavigationOption, show RSS feeds
+        //      The feedPosition is determined by taking position - 3 (number of NavigationOption)
+        if (position >= NavigationOption.values().length) {
+            int feedPosition = position - NavigationOption.values().length;
+            rssFeed = BloclyApplication.getSharedDataSource().getFeeds().get(feedPosition);
+        }
+        viewHolder.update(position, rssFeed);
     }
 
     // 43.4: getItemCount() will return number of NavigationOption w/ number of RSS feeds found from
@@ -44,6 +54,7 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
                 + BloclyApplication.getSharedDataSource().getFeeds().size();
     }
 
+    // 43.4: ViewHolder describes a single item view and metadata about its place w/in RecyclerView
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         View topPadding;
@@ -67,6 +78,36 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
         @Override
         public void onClick(View view) {
             Toast.makeText(view.getContext(), "Nothing... yet!", Toast.LENGTH_SHORT).show();
+        }
+
+        void update(int position, RssFeed rssFeed) {
+
+            // 43.4: Top padding is shown either at NAVIGATION_OPTION_INBOX (position 0)
+            //      or at the first feed item (position 3)
+            boolean shouldShowTopPadding = position == NavigationOption.NAVIGATION_OPTION_INBOX.ordinal()
+                    || position == NavigationOption.values().length;
+            topPadding.setVisibility(shouldShowTopPadding ? View.VISIBLE : View.GONE);
+
+            // 43.4: Bottom padding is shown either at NAVIGATION_OPTION_ARCHIVED (position 2)
+            //      or at getItemCount() - 1 (last Rss feed object)
+            boolean shouldShowBottomPadding = position == NavigationOption.NAVIGATION_OPTION_ARCHIVED.ordinal()
+                    || position == getItemCount() - 1;
+            bottomPadding.setVisibility(shouldShowBottomPadding ? View.VISIBLE : View.GONE);
+
+            // 43.4: Set divider visibility at NAVIGATION_OPTION_ARCHIVED.ordinal()
+            divider.setVisibility(position == NavigationOption.NAVIGATION_OPTION_ARCHIVED.ordinal()
+                    ? View.VISIBLE : View.GONE);
+
+            // 43.5: titleTexts[] represents the nav drawer options: Inbox, Favorites, Archived
+            if (position < NavigationOption.values().length) {
+                int[] titleTexts = new int[]{R.string.navigation_option_inbox,
+                        R.string.navigation_option_favorites,
+                        R.string.navigation_option_archived};
+
+                title.setText(titleTexts[position]);
+            } else {
+                title.setText(rssFeed.getTitle());
+            }
         }
     }
 }
